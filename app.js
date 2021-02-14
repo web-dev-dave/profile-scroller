fc9df8cdb4493389daaa736bdeb16995348ac670
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-15 11:35:57 +1300

    create data

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: '32',
+    gender: 'foo',
+    lookingfor: 'bar',
+    location: 'Auckland NZ',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+  {
+    name: 'Jen Smith',
+    age: '26',
+    gender: 'bar',
+    lookingfor: 'foo',
+    location: 'Wellington NZ',
+    image: 'https://randomuser.me/api/portraits/women/82.jpg'
+  },
+  {
+    name: 'William Johnson',
+    age: '38',
+    gender: 'foo',
+    lookingfor: 'bar',
+    location: 'Christchurch NZ',
+    image: 'https://randomuser.me/api/portraits/men/83.jpg'
+  }
+];
